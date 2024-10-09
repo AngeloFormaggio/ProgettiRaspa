d28e7479b8c7741ca6d8daf59778407a46443a85
Formaggio Angelo <angelo.formaggio@iisviolamarchesini.edu.it>
2024-10-09 21:02:26 +0200

    Aggiunti i commenti

--- Libreria/Es_Libreria.c
+++ Libreria/Es_Libreria.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_NOME 30
-#define MAX_AUTORE 20
-#define MAX_CAT 40
+#define MAX_NOME 30//dimensione massima del titolo/categoria
+#define MAX_AUTORE 20//dimensione massima autore
+#define MAX_CAT 40//numero massimo di categorie 
 #define BUFFER 512
 
 typedef struct{//struttura per un singolo libro
@@ -17,15 +17,15 @@ typedef struct{//struttura per un singolo libro
 typedef struct{//struttura per una singola categoria, ogni cat contiene un array di libri che ci sono all'interno
     Libro *libri;//array dinamico
     char nome[MAX_NOME];
-    int contLibri;
-    int maxLibri;
+    int contLibri;//libri contenuti
+    int maxLibri;//massimo di libri contenibili
 }Categoria;
 
 int CheckCategoria(Categoria categorie[], char *nomeCategoria, int nCat)
 {
     for(int i = 0; i < nCat; i++)
     {
-        if(strcmp(categorie[i].nome, nomeCategoria) == 0)
+        if(strcmp(categorie[i].nome, nomeCategoria) == 0)//controllo se il nome della cat. esiste gia
         {
             return i;//ritorno l'indice della categoria che gia esiste
         }
@@ -38,51 +38,51 @@ void AggiungiLibro(Categoria *categoria, char *bufTitolo, char *bufAutore, int b
     categoria->libri = (Libro *)realloc(categoria->libri, (categoria->maxLibri+1) *sizeof(Libro));//aumento l'array libri in modo da poter tenere un altro libro
     categoria->maxLibri++;
 
-    strcpy(categoria->libri[categoria->contLibri].titolo,bufTitolo);
+    strcpy(categoria->libri[categoria->contLibri].titolo,bufTitolo);//inserisco i dati nella struct
     strcpy(categoria->libri[categoria->contLibri].autore, bufAutore);
     categoria->libri[categoria->contLibri].anno = bufAnno;
     categoria->libri[categoria->contLibri].prezzo = bufPrezzo;
 
-    categoria->contLibri++;
+    categoria->contLibri++;//aumento il numero di libri contenuti
 }
 
 void AggiungiCategoria(Categoria categorie[], int *nCat, char *bufCategoria, char *bufTitolo, char *bufAutore, int bufAnno, float bufPrezzo) 
 {
-    strcpy(categorie[*nCat].nome, bufCategoria);
+    strcpy(categorie[*nCat].nome, bufCategoria);//copio il nome della cat. nella cat nuova
 
-    categorie[*nCat].maxLibri = 0;
+    categorie[*nCat].maxLibri = 0;//copio i dati nella cat. nuova
     categorie[*nCat].maxLibri++;
     categorie[*nCat].libri = malloc(categorie[*nCat].maxLibri * sizeof(Libro));//alloco una grandezza pari a 1 libro all'array Libro *libri(operazione eseguita solo per 1 array libri di una cat nuova, senno modifico la grandezza nel metodo aggiungi libri)
     categorie[*nCat].contLibri = 0;// inizializzo il contatore libir della categoria appena creata
 
-    AggiungiLibro(&categorie[*nCat], bufTitolo,bufAutore,bufAnno,bufPrezzo);
+    AggiungiLibro(&categorie[*nCat], bufTitolo,bufAutore,bufAnno,bufPrezzo);//chiamo la funzione per aggiungere un libro alla categoria appena creata
 
-    (*nCat)++;
+    (*nCat)++;//aumento il contatore delle cat. totali
 }
 
-void CaricaLibri(char *nomeFile, Categoria categorie[], int *nCat)
+void CaricaLibri(char *nomeFile, Categoria categorie[], int *nCat)//funzione per caricare i libri dal csv
 {
-    FILE *file = fopen(nomeFile, "r");
+    FILE *file = fopen(nomeFile, "r");//apro il file
     
-    if(file == NULL)
+    if(file == NULL)//controllo il file in apertura
     {
         printf("\nERRORE APERTURA FILE");
         exit(-1);
     }
 
-    char bufLinea[BUFFER];
+    char bufLinea[BUFFER];//buffer che contiene la riga letta
 
     fgets(bufLinea, BUFFER, file);//leggo e skippo la prima riga che è l'intestazione
 
     while(fgets(bufLinea,BUFFER,file))//finche non finisce il file leggo riga per riga, quando finisce fgets ritorna NULL
     {
-        char bufTitolo[MAX_NOME];
+        char bufTitolo[MAX_NOME];//dichhhiaro delle variabili di supporto
         char bufAutore[MAX_AUTORE];
         char bufCategoria[MAX_NOME];
         int bufAnno;
         float bufPrezzo;
 
-        char *token = strtok(bufLinea, ",");
+        char *token = strtok(bufLinea, ",");//ctreo un token per fare il parsing della stringa con le virgole e uso strtok
         if (token) {
             strncpy(bufTitolo, token, MAX_NOME);
             bufTitolo[MAX_NOME - 1] = '\0'; 
@@ -110,7 +110,7 @@ void CaricaLibri(char *nomeFile, Categoria categorie[], int *nCat)
             bufCategoria[MAX_NOME - 1] = '\0'; 
         }
 
-        int indexCat = CheckCategoria(categorie, bufCategoria, *nCat);
+        int indexCat = CheckCategoria(categorie, bufCategoria, *nCat);//controllo se la categoria esiste
 
         if(indexCat != -1)
         {//se c'è
@@ -124,29 +124,29 @@ void CaricaLibri(char *nomeFile, Categoria categorie[], int *nCat)
 
     }
 
-    fclose(file);
+    fclose(file);//chisura file
 
 
 }
 
-void VisualizzaXCategoria(Categoria categorie[], int nCat) 
+void VisualizzaXCategoria(Categoria categorie[], int nCat) //stampo tutti i libri
 {
     for (int i = 0; i < nCat; i++) {
         printf("Categoria: %s\n", categorie[i].nome);
         for (int j = 0; j < categorie[i].contLibri; j++) {
-            Libro *libro = &categorie[i].libri[j];
+            Libro *libro = &categorie[i].libri[j];//supporto
             printf("  Titolo: %s, Autore: %s, Anno: %d, Prezzo: %.2f\n", libro->titolo, libro->autore, libro->anno, libro->prezzo);
         }
     }
 }
 
-int CercaLibro(Categoria categorie[], int nCat)
+int CercaLibro(Categoria categorie[], int nCat)//ricerca di un libro
 {
     char titoloRicerca[MAX_NOME];
     printf("Inserire il titolo da cercare: ");
 
-    fgets(titoloRicerca, sizeof(titoloRicerca), stdin);
-    titoloRicerca[strcspn(titoloRicerca, "\n")] = '\0';
+    fgets(titoloRicerca, sizeof(titoloRicerca), stdin);//uso fgets, pk con scnaf e %s mi fermo al primo spazio
+    titoloRicerca[strcspn(titoloRicerca, "\n")] = '\0';//tolgo il carattere di fine dalla stringa
 
     for(int i = 0; i < nCat; i++)
     {
@@ -162,7 +162,7 @@ int CercaLibro(Categoria categorie[], int nCat)
     return 1;
 }
 
-void StampaXCategoria(Categoria categorie[],int nCat)
+void StampaXCategoria(Categoria categorie[],int nCat)//stampo tutti i libri di una categoria scelta
 { 
     int scelta;
 
@@ -195,7 +195,7 @@ int main(int argc, char* argv[])
     do{
         printf("Scegli opzione:\n1-Visualizza libri x Cat\n2-Ricerca libro\n3-Stampa x categoria\n");
         scanf("%d",&scelta);
-        getchar();
+        getchar();//devo svuotare il buffer dopo la scanf, senno da problemi quando uso fgets in verca libro
 
         switch (scelta) 
         {
